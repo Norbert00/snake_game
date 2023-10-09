afa7c11fc0b80f0ed0e8b2c91a6ccda542288f5d
Norbert00 <norbert.jurek@protonmail.com>
2023-10-09 12:55:33 +0200

    The rest of the game logic, ways to end the game have been implemented

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import time
 from snake import Snake
 from game_screen import GameScreen
+from food import Food
+from scoreboard import Scoreboard
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 gameScreen = GameScreen(snake)
 gameScreen.game_screen_trace()
 gameScreen.listen_on_keys()
@@ -14,6 +18,20 @@ def game():
         gameScreen.game_screen_update()
         time.sleep(0.1)
         snake.move()
+
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            scoreboard.update_score()
+            snake.extend()
+        if snake.head.xcor() > 298 or snake.head.xcor() < -298 or snake.head.ycor() > 298 or snake.head.ycor() < -298:
+            game_is_on = False
+            scoreboard.game_over()
+        for segment in snake.segments:
+            if segment == snake.head:
+                pass
+            elif snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
 
 
 game()
